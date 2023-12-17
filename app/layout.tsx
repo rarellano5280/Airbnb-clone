dcacb93da7d15b90d39688fa160457c632375fa2
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-12-17 08:56:35 -0700

    added button and modal logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import './globals.css'
-import Navbar from './components/Navbar/Navbar'
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
+import Modal from "./components/Modals/modals";
 
-
-const nunito = Nunito({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb clone',
-}
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Navbar/>
+        <Modal actionLablel="Submit"title="Hello World"isOpen />
+        <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
